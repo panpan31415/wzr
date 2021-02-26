b9aa31444bb53ede2a9dcdbaf50d81f016b703fd
panpan31415 <panpan31415@gmail.com>
2021-02-26 01:41:45 +0100

    add exercise 4

---
+++ exercise4.py
@@ -0,0 +1,16 @@
+# write a console program to simulate calculator
+#
+# if you type 1+2*3-4 in a python environment,
+# it will print out 3
+#
+# you task is to write a program to
+# 1. ask user to input a string, which means should use quotes to wrap
+#   input, for example: if you want to calculate 1+1, you should type
+#   "1+1"
+#
+# 2. interpret user input, and calculate the result
+#
+# 3. only support "+","-","*","/" and integers in the calculation,
+#    don't use space or ".", nor other charaters in user input. Otherwise,
+#    it will be extremely hard
+#
